bd8d2375075f114e7b9c30ec2853803f2ce7d598
Elena Tanasoiu <elena.tanasoiu@gmail.com>
2020-07-21 14:47:41 +0100

    Rename hello world to welcome action
    This is an action we want to keep. We're renaming it "Welcome". You
    can use this action to test out how Squiddy works.
    
    Since Squiddy is open-source we've included a link to our tech blog
    in case people are interested in reading more about FutureLearn.
    
    Co-authored-by: Marco Tranchino <marco.tranchino@futurelearn.com>

--- actions/hello_world/hello_world.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-
-puts "Hello world my name is #{ARGV[0]}!"
-
-puts "This is github_event: #{ARGV[1]}"
---
+++ actions/welcome/welcome.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+
+puts "Welcome to the Squiddy repo!"
+puts "If you want to read more about how we are building FutureLearn you can visit our tech blog https://medium.com/@FutureLearn"
