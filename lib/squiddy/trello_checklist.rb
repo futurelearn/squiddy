6524adc35b7b68e90b205f0b6528656c78e04775
Laura Martin <surminus@gmail.com>
2020-07-22 15:25:01 +0100

    Add the rest of the methods
    We have the following methods which allow are to interact with Trello:
    
    - create_checklist
    - add_item
    - mark_item_as_complete
    
    The Trello credentials are passed in either when initializing the class
    or using environment variables.

--- lib/squiddy/trello_checklist.rb
+++ lib/squiddy/trello_checklist.rb
@@ -1,27 +1,54 @@
+require 'trello'
+
 module Squiddy
   class TrelloChecklist
-    attr_reader :card_url
+    class CardNotFound < StandardError; end
+    class ChecklistNotFound < StandardError; end
 
-    def initialize(card_url)
+    attr_reader :card_url, :client
+
+    def initialize(card_url, developer_public_key: nil, member_token: nil)
       @card_url = card_url
+
+      @client = Trello::Client.new(
+        developer_public_key: developer_public_key || ENV['SQUIDDY_TRELLO_DEVELOPER_PUBLIC_KEY'],
+        member_token: member_token || ENV['SQUIDDY_TRELLO_MEMBER_TOKEN']
+      )
     end
 
     def create_checklist
+      return checklist if checklist_exist?
+
+      client.create(:checklist, name: "Pull Requests", "idCard" => card.id)
     end
 
     def checklist_exist?
+      !checklist.nil?
     end
 
     def add_item(item)
+      fail ChecklistNotFound unless checklist_exist?
+
+      resp = checklist.add_item(item)
+
+      resp.code == 200
     end
 
     def mark_item_as_complete(item)
+      fail ChecklistNotFound unless checklist_exist?
+
+      id = item_id(item)
+
+      checklist.update_item_state(id, "complete")
+      checklist.save
+
+      true
     end
 
-    def card_id_from_url(url)
-      fail StandardError, "Malformed URL" unless url =~ /#{URI::regexp(['http', 'https'])}/
+    def card_id_from_url
+      fail StandardError, "Malformed URL" unless card_url =~ /#{URI::regexp(['http', 'https'])}/
 
-      uri = URI.parse(url)
+      uri = URI.parse(card_url)
       path = uri.path
 
       # Trello cards look like https://trello.com/c/card-id
@@ -29,6 +56,20 @@ module Squiddy
       fail CardNotFound unless path =~ /^\/c\/.*$/
 
       path.split("/").reject { |p| p.empty? }[1]
+    end
+
+    private
+
+    def card
+      client.find(:card, card_id_from_url)
+    end
+
+    def checklist
+      card.checklists.find { |checklist| checklist.name == "Pull Requests" }
+    end
+
+    def item_id(name)
+      card.checklists.first.items.find { |item| item.name == name }.id
     end
   end
 end
--- spec/lib/squiddy/trello_checklist_spec.rb
+++ spec/lib/squiddy/trello_checklist_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'rest-client'
 
 require_relative '../../../lib/squiddy/trello_checklist'
 
@@ -6,41 +7,71 @@ RSpec.describe Squiddy::TrelloChecklist do
   subject { described_class.new("https://trello.com/c/1234546789") }
 
   context 'the checklist does not exist' do
+    let(:trello_checklist) { instance_double(Trello::Checklist) }
+    let(:trello_card) { instance_double(Trello::Card, checklists: [], id: 'a-long-card-id') }
+    let(:client) { double(Trello::Client, find: trello_card) }
+
+    before do
+      allow(Trello::Client).to receive(:new).and_return(client)
+      allow(client).to receive(:find).with(:card, "https://trello.com/c/1234546789").and_return(trello_card)
+      allow(client).to receive(:create).with(:checklist, name: "Pull Requests", "idCard" => "a-long-card-id")
+        .and_return(trello_checklist)
+      allow(trello_checklist).to receive(:add_item).and_return(200)
+    end
+
     describe '#create_checklist' do
-      it 'creates a checklist on the card and returns true' do
-        expect(subject.create).to eq(true)
+      it 'creates a checklist on the card and return the checklist object' do
+        expect(subject.create_checklist).to eq(trello_checklist)
       end
     end
 
     describe '#checklist_exist?' do
       it 'returns false' do
-        expect(subject.exist?).to eq(false)
+        expect(subject.checklist_exist?).to eq(false)
       end
     end
 
     describe '#add_item' do
-      it 'returns true when the item is added' do
-        expect(subject.add_item('test')).to eq(true)
+      it 'raises an error' do
+        expect { subject.add_item('test') }.to raise_error(Squiddy::TrelloChecklist::ChecklistNotFound)
       end
     end
 
     describe '#mark_item_as_complete' do
-      it 'returns true when the item is marked as complete' do
-        expect(subject.mark_item_as_complete('test')).to eq(true)
+      it 'raises an error' do
+        expect { subject.add_item('test') }.to raise_error(Squiddy::TrelloChecklist::ChecklistNotFound)
       end
     end
   end
 
   context 'the checklist exists' do
+    let(:trello_item) { instance_double(Trello::Item, name: "test", id: "1234") }
+    let(:rest_response) { instance_double(RestClient::Response) }
+    let(:trello_checklist) { instance_double(Trello::Checklist, name: "Pull Requests", add_item: rest_response) }
+    let(:trello_card) { instance_double(Trello::Card, checklists: [trello_checklist], id: 'a-long-card-id') }
+    let(:client) { double(Trello::Client, find: trello_card) }
+
+    before do
+      allow(Trello::Client).to receive(:new).and_return(client)
+      allow(client).to receive(:find).with(:card, "https://trello.com/c/1234546789").and_return(trello_card)
+      allow(client).to receive(:create).with(:checklist, name: "Pull Requests", "idCard" => "a-long-card-id")
+        .and_return(trello_checklist)
+      allow(trello_checklist).to receive(:add_item).and_return(rest_response)
+      allow(trello_checklist).to receive(:items).and_return([trello_item])
+      allow(trello_checklist).to receive(:update_item_state).and_return(trello_checklist)
+      allow(trello_checklist).to receive(:save).and_return(trello_checklist)
+      allow(rest_response).to receive(:code).and_return(200)
+    end
+
     describe '#create_checklist' do
       it 'returns false' do
-        expect(subject.create).to eq(false)
+        expect(subject.create_checklist).to eq(trello_checklist)
       end
     end
 
     describe '#checklist_exist?' do
       it 'returns true' do
-        expect(subject.exist?).to eq(true)
+        expect(subject.checklist_exist?).to eq(true)
       end
     end
 
@@ -60,19 +91,18 @@ RSpec.describe Squiddy::TrelloChecklist do
   describe '#card_id_from_url' do
     let(:good_url) { "https://trello.com/c/1234abcd/good-card" }
     let(:bad_url) { "https://trello.com/b/1234abcd/bad-board" }
-    let(:non_existent_url) { "https://trello.com/c/12345678/bad-card" }
     let(:non_url) { "some-string" }
 
     it 'returns the card ID from a given Trello card URL' do
-      expect(subject.card_id_from_url(good_url)).to eq('1234abcd')
+      expect(Squiddy::TrelloChecklist.new(good_url).card_id_from_url).to eq('1234abcd')
     end
 
     it 'returns an error when given a non-card URL' do
-      expect { subject.card_id_from_url(bad_url) }.to raise_error(Squiddy::TrelloChecklist::CardNotFound)
+      expect { Squiddy::TrelloChecklist.new(bad_url).card_id_from_url }.to raise_error(Squiddy::TrelloChecklist::CardNotFound)
     end
 
     it 'returns an error when not a URL' do
-      expect { subject.card_id_from_url(non_url) }.to raise_error(StandardError)
+      expect { Squiddy::TrelloChecklist.new(non_url).card_id_from_url }.to raise_error(StandardError)
     end
   end
 end
