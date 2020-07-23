e67fd127877ace75fd487b3334e5d060cc30c7e5
Laura Martin <surminus@gmail.com>
2020-07-23 14:56:44 +0100

    Add TrelloPullRequest class
    This adds the first top level action, which can be directly triggered,
    and puts together the logic for running the action.
    
    I haven't yet added any checks in for required environment variables,
    such as required API keys, since I will write those checks into the
    entrypoint script that will trigger the run.

--- lib/squiddy.rb
+++ lib/squiddy.rb
@@ -1,2 +1,39 @@
+require_relative 'squiddy/event'
+require_relative 'squiddy/pull_request'
+require_relative 'squiddy/trello_checklist'
+
 module Squiddy
+  # TrelloPullRequest automatically creates a checklist on a Trello card called
+  # "Pull Requests", and adds the pull request URL as an item.
+  #
+  # If the Pull Request is in a closed state, then it marks the item as
+  # complete
+  class TrelloPullRequest
+    def self.run
+      event = Squiddy::Event.new
+
+      return nil unless event.type == "pull_request"
+
+      pull_request = Squiddy::PullRequest.new(event.repository, event.pull_request_number)
+
+      trello_regex = /https:\/\/trello\.com\/c\/\w+/
+
+      trello_card = pull_request.body_regex(trello_regex)
+      return nil unless trello_card
+
+      trello = Squiddy::TrelloChecklist.new(trello_card)
+
+      item = pull_request.url
+
+      if pull_request.open?
+        trello.create_checklist unless trello.checklist_exist?
+
+        trello.add_item(item) unless trello.item_exist?(item)
+      end
+
+      if pull_request.closed?
+        trello.mark_item_as_complete(item) if trello.item_exist?(item)
+      end
+    end
+  end
 end
