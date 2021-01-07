3a3ee2730894697788ca4059e707aefc1d2bb6e0
Elena Tanasoiu <elena.tanasoiu@gmail.com>
2021-01-07 17:30:18 +0000

    Rename repo to repo_name
    This contains the name of the repo, rather than the repo object.

--- actions/bubble_merge/git_client.rb
+++ actions/bubble_merge/git_client.rb
@@ -3,7 +3,7 @@ require "json"
 
 module Squiddy
   class GitClient
-    attr_reader :client, :event, :pr, :repo
+    attr_reader :client, :event, :pr
 
     CHECK_STATUSES = %w[queued in_progress completed].freeze
     CHECK_CONCLUSIONS = %w[success failure neutral cancelled skipped timed_out action_required stale].freeze
@@ -15,13 +15,12 @@ module Squiddy
 
       @client = Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"])
       @event = git_event
-      @repo = repo_name
       @pr = pull_request
     end
 
     def bubble_merge
       if already_merged?
-        client.add_comment(repo, pr_number, 'This PR is already merged.')
+        client.add_comment(repo_name, pr_number, 'This PR is already merged.')
       else
         merge_and_close_pr
       end
@@ -30,7 +29,7 @@ module Squiddy
     private
 
     def already_merged?
-      client.pull_merged?(repo, pr_number)
+      client.pull_merged?(repo_name, pr_number)
     end
 
     def merge_and_close_pr
@@ -43,12 +42,12 @@ module Squiddy
         #### Error message:
         #{e&.message}
       MESSAGE
-      client.add_comment(repo, pr_number, message)
+      client.add_comment(repo_name, pr_number, message)
     end
 
     def merge
       client.merge(
-        repo,
+        repo_name,
         base_branch,
         branch,
         {
@@ -60,11 +59,11 @@ module Squiddy
     end
 
     def delete_branch
-      client.delete_branch(repo, branch)
+      client.delete_branch(repo_name, branch)
     end
 
     def main_sha
-      client.list_commits(repo).last.sha
+      client.list_commits(repo_name).last.sha
     end
 
     def branch
@@ -92,11 +91,11 @@ module Squiddy
     end
 
     def head_commit_checks
-      client.check_suites_for_ref(repo, head_commit_sha)
+      client.check_suites_for_ref(repo_name, head_commit_sha)
     end
 
     def head_commit_sha
-      client.pull_request_commits(repo, pr_number).last.sha
+      client.pull_request_commits(repo_name, pr_number).last.sha
     end
 
     def comment_author
@@ -147,7 +146,7 @@ module Squiddy
     end
 
     def pull_request
-      client.pull_request(repo, pr_number)
+      client.pull_request(repo_name, pr_number)
     end
   end
 end
